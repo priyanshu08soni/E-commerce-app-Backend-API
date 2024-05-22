e29edefeaefd33ff70cee3543168e31a3d3c5fd1
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-22 13:16:23 +0530

    asdfsfafdsfasdfasd

--- config/axiosconfig.js
+++ config/axiosconfig.js
@@ -2,6 +2,5 @@ export const config={
     headers:{
         origin:"https://e-commerce-app-frontend-pink.vercel.app",
         methods:["GET", "POST", "PATCH", "PUT", "DELETE"," OPTIONS", "OPTNS"],
-        status:204
     }
 }
