5496613da5e839d0063396ef58fee2619364eee7
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-22 10:00:06 +0530

    ch

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -12,17 +12,11 @@ router.put(
     uploadPhoto.array("images", 10),
     blogImgResize,
     uploadImages
-);
-const config = {
-    headers: {
-      "Access-Control-Allow-Origin": "https://e-commerce-app-frontend-pink.vercel.app",
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-    }
-};
+  );
 router.put('/dislikes',authMiddleware,dislikeBlog);
 router.put('/likes',authMiddleware,likeBlog);
 router.put('/:id',authMiddleware,isAdmin,updateBlog);
-router.get('/:id',getaBlog,config);
-router.get('/',getAllBlogs,config);
+router.get('/:id',getaBlog);
+router.get('/',getAllBlogs);
 router.delete('/:id',authMiddleware,isAdmin,deleteBlog);
 module.exports=router;
