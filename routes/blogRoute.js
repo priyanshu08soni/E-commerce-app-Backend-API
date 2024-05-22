526813a73b99c424f588a569de8fe5ecf82b2704
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-22 16:32:05 +0530

    change

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -3,9 +3,8 @@ const router=express.Router();
 const {authMiddleware, isAdmin}=require('../middlewares/authMiddlewares');
 const { createBlog, updateBlog, getaBlog, getAllBlogs, deleteBlog, likeBlog, dislikeBlog, uploadImages } = require('../controller/blogCtrl');
 const { blogImgResize, uploadPhoto } = require('../middlewares/uploadImages');
+
 //post->put->get->delete
-const headers=new Headers();
-headers.append('Access-Control-Allow-Origin','https://e-commerce-app-frontend-pink.vercel.app');
 router.post('/',authMiddleware,isAdmin,createBlog);
 router.put(
     "/upload/:id",
@@ -19,6 +18,6 @@ router.put('/dislikes',authMiddleware,dislikeBlog);
 router.put('/likes',authMiddleware,likeBlog);
 router.put('/:id',authMiddleware,isAdmin,updateBlog);
 router.get('/:id',getaBlog);
-router.get('/',getAllBlogs,headers);
+router.get('/',getAllBlogs);
 router.delete('/:id',authMiddleware,isAdmin,deleteBlog);
 module.exports=router;
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -9,13 +9,13 @@ const {
   rating,
 } = require("../controller/ProductCtrl");
 const { isAdmin, authMiddleware } = require("../middlewares/authMiddlewares");
-const { config } = require("../config/axiosconfig");
+
 
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createProduct);
 
-router.get("/", getAllProduct,config);
+router.get("/", getAllProduct);
 router.get("/:id", getaProduct);
 router.put("/wishlist", authMiddleware, addToWishlist);
 router.put("/rating", authMiddleware, rating);
