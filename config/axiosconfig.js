e0cc7bcb43a27b38ac9ded9a86a20ba2ebd90ad8
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-22 13:45:14 +0530

    wsswsws

--- config/axiosconfig.js
+++ config/axiosconfig.js
@@ -1,6 +1,7 @@
 export const config={
     headers:{
         origin:"https://e-commerce-app-frontend-pink.vercel.app",
+        "Access-Control-Allow-Origin":"https://e-commerce-app-frontend-pink.vercel.app",
         methods:["GET", "POST", "PATCH", "PUT", "DELETE"," OPTIONS", "OPTNS"]
     }
 }
